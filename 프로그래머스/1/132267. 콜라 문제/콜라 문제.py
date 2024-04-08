dbe8276017de27c036e62962beafa7a51bc4144f
kybcod <148421339+kybcod@users.noreply.github.com>
2024-04-09 08:32:29 +0900

    [level 1] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,6 +1,7 @@
 def solution(a, b, n):
     answer = 0
     while n >= a:
+        # new : 콜라 빈 병의 개수를 가져다주면서 얻을 수 있는 새로운 콜라의 개수
         new = (n // a) * b
         left = n % a
         answer += new 
