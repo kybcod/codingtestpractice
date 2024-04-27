d4ec8160f336c2b0f7b7d7e66081ac3d039d19b8
kybcod <148421339+kybcod@users.noreply.github.com>
2024-04-27 23:51:16 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,19 +1,15 @@
 a, b, c = map(int, input().split())
-reward = 0
 
-if a == b == c:  # 모두 같은 경우
+if a == b == c:
     reward = 10000 + a * 1000
-elif a == b or a == c or b == c:  # 두 개의 눈이 같은 경우
-    if a == b == c:
-        reward = 10000 + a * 1000
-    else:
-        if a == b:
-            reward = 1000 + a * 100
-        elif a == c:
-            reward = 1000 + a * 100
-        elif b == c:
-            reward = 1000 + b * 100
-else:  # 모두 다른 경우
+elif a == b or b == c or a == c:
+    if a == b:
+        reward = 1000 + a * 100
+    elif b == c:
+        reward = 1000 + b * 100
+    elif a == c:
+        reward = 1000 + c * 100
+else:
     reward = max(a, b, c) * 100
 
 print(reward)
