46db782fb313b8c693da9b09085de72ae11a4841
kybcod <148421339+kybcod@users.noreply.github.com>
2024-04-28 20:55:14 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,13 +1,2 @@
-cnt = 0
-
-n = list(int(input()) for _ in range(10))
-answer = []
-
-for num in n:
-    a = num % 42
-    answer.append(a)
-    
-    
-for i in set(answer):
-    cnt += 1
-print(cnt)
+n = [int(input()) % 42 for _ in range(10)]
+print(len(set(n)))
