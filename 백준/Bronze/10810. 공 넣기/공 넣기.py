0fefc2f3a1f85935aef059d873ff6417007ce823
kybcod <148421339+kybcod@users.noreply.github.com>
2024-04-28 20:33:41 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -3,8 +3,8 @@ baskets = [0] * n
 
 for _ in range(m):
     i,j,k = map(int, input().split())
-    for i in range(i-1,j):
-        baskets[i] = k
+    for a in range(i-1,j):
+        baskets[a] = k
         
 print(*baskets)
 # print(baskets[0], baskets[1], ..., baskets[N-1])
