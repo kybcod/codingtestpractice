16ef86bddcc8e1dbdf7c27114b1404fe46e70bf2
yebeen <148421339+kybcod@users.noreply.github.com>
2025-01-27 16:10:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,32 +1,19 @@
 def solution(s):
+    words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    word = ''
+    answer = ''
+    i = 0
 
-    dic = {
-        'zero' : '0',
-        'one' : '1',
-        'two' : '2',
-        'three' : '3',
-        'four' : '4',
-        'five' : '5',
-        'six' : '6',
-        'seven' : '7',
-        'eight' : '8',
-        'nine' : '9'
-    }
-    
-    answer = s
-    for key, value in dic.items():
-        answer = answer.replace(key, value)
-        
-    # answer = ''
-    # word = ''
+    while i < len(s):
+        if s[i].isdigit(): 
+            answer += s[i]
+            i += 1
+            continue
 
-#     for i in s:
-#         if i.isalpha():
-#             word += i
-#             if word in dic:
-#                 answer += dic[word]
-#                 word = ''
-#         else:
-#             answer += i
+        word += s[i]  
+        if word in words:  
+            answer += str(words.index(word))
+            word = ''  
+        i += 1
 
     return int(answer)
