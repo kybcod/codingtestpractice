466be0775c8df17757573f135725a34c9e68a3c9
yebeen <148421339+kybcod@users.noreply.github.com>
2025-01-31 21:10:41 +0900

    [level 1] Title: 콜라 문제, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,9 +1,10 @@
 def solution(a, b, n):
     answer = 0
-    while n >= a:
-        # new : 콜라 빈 병의 개수를 가져다주면서 얻을 수 있는 새로운 콜라의 개수
-        new = (n // a) * b
-        left = n % a
-        answer += new 
-        n = new + left
+    # 단, 보유 중인 빈 병이 2개 미만이면, 콜라를 받을 수 없다.
+    # 빈 병의 개수가 콜라를 받기 위해서 필요한 개수보다 크면 반복한다
+    while (n >= a):
+        remain_bottle = n % a
+        n = (n//a) * b # 마트에서 받은 콜라의 수
+        answer += n # 받은 걸 answer에 +
+        n += remain_bottle # 남아있는 병을 더해줘서 다음에 마트갈 때 이용
     return answer
