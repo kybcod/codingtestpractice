a427f9ebeabdf148e0e9ff969daf683a936386c4
yebeen <148421339+kybcod@users.noreply.github.com>
2025-02-03 16:09:32 +0900

    [level 1] Title: 추억 점수, Time: 2.37 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,15 +1,10 @@
 def solution(name, yearning, photo):
     answer = []
-    dic = {}
-
-    for yearn_name, yearn in zip(name, yearning):
-        dic[yearn_name] = yearn
-
-    for i in photo:
-        sum = 0
-        for j in i:
-            if j in dic:
-                sum += dic[j]
-        answer.append(sum)
-            
+    name_dict = dict(zip(name, yearning))
+    
+    for p in photo:
+        total = 0
+        for person in p:
+            total += name_dict.get(person, 0)
+        answer.append(total)
     return answer
