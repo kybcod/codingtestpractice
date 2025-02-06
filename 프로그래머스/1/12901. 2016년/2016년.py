e2bab684d9c589e340ab9220cc098adc6d305ad1
yebeen <148421339+kybcod@users.noreply.github.com>
2025-02-06 09:33:11 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,6 +1,5 @@
 def solution(a, b):
-    week = ['THU', 'FRI', 'SAT', 'SUN','MON','TUE','WED'] 
-    month = [31,29,31,30,31,30,31,31,30,31,30,31]
-
+    days = ['THU', 'FRI', 'SAT','SUN', 'MON', 'TUE','WED']
+    months = [31,29,31,30,31,30, 31, 31, 30, 31, 30, 31]
     
-    return week[(sum(month[:a-1])+b) % 7]
+    return days[(sum(months[:a-1])+b)%7]
