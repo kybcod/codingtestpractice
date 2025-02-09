605c1a074588260c5f2782b2aa4dd1a043b50e4c
yebeen <148421339+kybcod@users.noreply.github.com>
2025-02-09 21:32:09 +0900

    [level 1] Title: 과일 장수, Time: 153.96 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/README.md
+++ 프로그래머스/1/135808. 과일 장수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 과일 장수 - 135808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=python3) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 0.03 ms
+메모리: 21.5 MB, 시간: 153.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 25일 09:54:42
+2025년 02월 09일 21:32:05
 
 ### 문제 설명
 
--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,12 +1,7 @@
-def solution(k, m, score):
-    answer = 0 
-    score = sorted(score, reverse = True)
-    
+def solution(k, m, score): # 사과 최대 점수, 한 상자 사과 개수, 사과 점수
+    answer = 0 # 이익
+    score.sort(reverse=True)
     for i in range(0, len(score), m):
-        tmp = score[i:i+m]
-        
-        if len(tmp) == m:
-            answer += min(tmp) * m
-        
-        
+        if len(score[i:i+m]) == m:
+            answer += min(score[i:i+m]) * m
     return answer
