aa50e3744e93816ebf962c436793bd4e46157aae
yebeen <148421339+kybcod@users.noreply.github.com>
2025-02-10 12:55:17 +0900

    [level 1] Title: 소수 찾기, Time: 3068.58 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,18 +1,13 @@
 import math
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
 
 def solution(n):
-    result = []
-
-    #모든 정수 반복(2~n-1까지)
-    for num in range(2,n+1): 
-        is_prime = True #소수인지 아닌지 판별을 위해 선언
-        
-        # num = 2,3일 때는 range(2, 2)가 되므로 반복문 실행X 
-        # is_prime = True 유지
-        for i in range(2, int(math.sqrt(num))+1):
-            if num % i == 0:
-                is_prime = False #소수 아님
-                break
-        if is_prime: #소수가 참이면
-            result.append(num) #리스트에 저장
-    return len(result)
+    answer = 0
+    for i in range(2, n+1):
+        if isPrime(i) == True:
+            answer += 1
+    return answer
