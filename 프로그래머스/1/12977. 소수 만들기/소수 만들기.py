c1ec17aff547bd7541be53fa1ae5cec56c41357a
yebeen <148421339+kybcod@users.noreply.github.com>
2025-02-11 09:23:11 +0900

    [level 1] Title: 소수 만들기, Time: 200.64 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,24 +1,20 @@
-import math
 from itertools import combinations
 
+#소수 찾기
+def is_Prime(n):
+    for j in range(2,n):
+        if n % j == 0:
+            return False
+    return True
+
+
 def solution(nums):
-    result = []
-    com = list(combinations(nums,3))
-    for i in com:
-        num = sum(i)
-        print(num)
+    sums = [sum(i) for i in combinations(nums, 3)]
+    
+    answer = 0
+    for i in sums:
+        isPrime = is_Prime(i)
+        if isPrime:
+            answer += 1
 
-        #모든 정수 반복(2~n-1까지)
-        # for num in range(2,n+1): 
-        is_prime = True
-
-        # num = 2,3일 때는 range(2, 2)가 되므로 반복문 실행X 
-        # is_prime = True 유지
-        for i in range(2, int(math.sqrt(num))+1):
-            if num % i == 0:
-                is_prime = False #소수 아님
-                break
-        if is_prime: #소수가 참이면
-            result.append(num) #리스트에 저장
-
-    return len(result)
+    return answer
