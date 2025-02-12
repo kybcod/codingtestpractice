9c7494732141741c9c246d1708139f614c9e03e4
yebeen <148421339+kybcod@users.noreply.github.com>
2025-02-12 11:13:54 +0900

    [level 1] Title: 옹알이 (2), Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,15 +1,12 @@
 def solution(babbling):
-    word = ["aya", "ye", "woo", "ma"]
-    cnt = 0
-
-    for i in babbling:
-        for j in word:
-            
-            # 단어가 연속으로 두 번 이상 나오지 않으면
-            if j * 2 not in i:
-                i = i.replace(j," ") #해당 단어를 해당 문자열에 제거
-                
-        # 문자열이 공백이 되면 모든 단어가 제거 후 cnt +1   
-        if i.strip() == "":
-            cnt += 1
-    return cnt
+    answer = 0
+    li = ["aya", "ye", "woo", "ma"]
+    
+    for b in babbling:
+        for l in li:
+            if l * 2 not in b:
+                b = b.replace(l, ' ')
+        
+        if b.isspace():
+            answer += 1
+    return answer
