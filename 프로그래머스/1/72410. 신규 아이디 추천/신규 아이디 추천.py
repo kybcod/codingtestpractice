23b439b6758827593ac90d32739054b8e91f7d03
yebeen <148421339+kybcod@users.noreply.github.com>
2025-10-06 17:51:20 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.30 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -3,8 +3,8 @@ import re
 def solution(new_id):
     
     new_id = new_id.lower() # 1단계: 소문자 치환
-    new_id = re.sub(r'[^a-z0-9\-_.]', '', new_id) # 2단계: 허용 문자 제외 제거
-    new_id = re.sub(r'\.+', '.', new_id) # 3단계: 마침표 2번 이상 → 하나로
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id) # 2단계: 허용 문자 제외 제거
+    new_id = re.sub('\.+', '.', new_id) # 3단계: 마침표 2번 이상 → 하나로
     new_id = new_id.strip('.') # 4단계: 처음/끝 마침표 제거
     
     if not new_id: # 5단계: 빈 문자열이면 'a'
