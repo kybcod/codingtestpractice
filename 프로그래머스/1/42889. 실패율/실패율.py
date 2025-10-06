891bea8461621a02cba5720553c630e98477f2cb
yebeen <148421339+kybcod@users.noreply.github.com>
2025-10-06 22:07:44 +0900

    [level 1] Title: 실패율, Time: 1941.01 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,22 +1,23 @@
-from fractions import Fraction
-
 def solution(N, stages):
-
-    fail = []  # 실패율 저장 리스트
-
-    # 각 스테이지별 실패율 계산
+    answer = []
+    length = len(stages)  # 전체 스테이지 도달자 수 (즉, 총 사용자 수)
+    
+    # 1번부터 N번 스테이지까지 반복
     for i in range(1, N+1):
-        challengers = len([x for x in stages if x >= i])  # 해당 스테이지 이상 도달한 사용자 수
-        failures = stages.count(i)  # 해당 스테이지에서 멈춰 있는 사용자 수
-
-        if challengers == 0:  # 도달한 플레이어가 없으면 실패율 0
-            fail.append((i, Fraction(0)))  # (스테이지 번호, 실패율)
+        cnt = stages.count(i)  # 현재 스테이지 i에 머물러 있는 사람 수 (즉, 실패한 사람 수)
+        
+        if length == 0:  # 만약 지금까지 남은 사람이 없으면 실패율 0
+            failure_rate = 0
         else:
-            fail.append((i, Fraction(failures, challengers)))  # (스테이지 번호, 실패율)
-
-    # 실패율 기준 내림차순 정렬 (실패율이 같으면 스테이지 번호 오름차순)
-    fail.sort(key=lambda x: x[1], reverse=True)
-
-    # 정렬된 스테이지 번호 리스트 출력
-    sorted_stages = [stage[0] for stage in fail]
-    return(sorted_stages) 
+            failure_rate = cnt / length  # 실패율 = 해당 스테이지 실패자 수 / 그 스테이지에 도달한 사람 수
+        
+        answer.append((i, failure_rate))  # 스테이지 번호와 실패율을 튜플로 리스트에 추가
+        length -= cnt  # 다음 스테이지 도달자 수 = 현재 남은 사람 수 - 현재 스테이지에서 실패한 사람 수
+    
+    # 실패율 기준으로 내림차순 정렬
+    answer = sorted(answer, key=lambda x: x[1], reverse=True)
+    
+    # 스테이지 번호만 추출해서 반환
+    answer = [i[0] for i in answer]
+    
+    return answer
